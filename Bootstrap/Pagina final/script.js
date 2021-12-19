589d4310ecc80d5c5262531b1d4285dda873eaf1
RubenGonz <ruben30303030@gmail.com>
2021-12-19 20:38:12 +0000

    Página de la música responsiva

--- Bootstrap/Pagina final/script.js
+++ Bootstrap/Pagina final/script.js
@@ -16,3 +16,11 @@ const ajustarPantalla = () => {
         elemento.style.height = alturaCuerpo;
     }
 }
+
+document.getElementById("botonOpciones").addEventListener("click", () => {
+    if (document.getElementById("listaOpciones").classList.contains("d-none")) {
+        document.getElementById("listaOpciones").classList.remove("d-none");
+    } else {
+        document.getElementById("listaOpciones").classList.add("d-none");
+    }
+});
