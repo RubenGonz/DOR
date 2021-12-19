c35beee92525ea6c5ae21d07685b16db9cbef94f
RubenGonz <ruben30303030@gmail.com>
2021-12-19 21:18:05 +0000

    Explicación de los detalles de la pagina de Music Zone

--- Bootstrap/Pagina final/script.js
+++ Bootstrap/Pagina final/script.js
@@ -1,22 +1,3 @@
-onload = () => {
-    ajustarPantalla();
-}
-
-onresize = () => {
-    ajustarPantalla();
-}
-
-const ajustarPantalla = () => {
-    let alturaPagina = document.body.clientHeight;
-    let alturaCabecera = document.getElementsByTagName("header")[0].clientHeight;
-    let alturaFooter = document.getElementsByTagName("footer")[0].clientHeight;
-    let elementos = document.getElementsByClassName("scrollActivado");
-    for (const elemento of elementos) {
-        let alturaCuerpo = alturaPagina - alturaCabecera - alturaFooter;
-        elemento.style.height = alturaCuerpo;
-    }
-}
-
 document.getElementById("botonOpciones").addEventListener("click", () => {
     if (document.getElementById("listaOpciones").classList.contains("d-none")) {
         document.getElementById("listaOpciones").classList.remove("d-none");
